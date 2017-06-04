3acdeac7e5ad5c80da2f3d31b569a17b9947db6b
Tyler D. Franklin <irishlep84@gmail.com>
2017-06-03 23:03:18 -0500

    Assignment 19 Complete

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,26 @@
+class AdvertisementsController < ApplicationController
+  def index
+    @advertisements = Advertisement.all
+  end
+
+  def show
+    @advertisement = Advertisement.find(params[:id])
+  end
+
+  def new
+    @advertisement = Advertisement.new
+  end
+
+  def create
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:advertisement][:title]
+    @advertisement.body = params[:advertisement][:body]
+    if @advertisement.save
+      flash[:notice] = "Advertisement was saved."
+      redirect_to @advertisement
+    else
+      flash.now[:alert] = "There was an error saving the post.  Please try again."
+      render :new
+    end
+  end
+end
