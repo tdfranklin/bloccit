45664e896382946725cef971832b2f1102c3d2d2
Tyler D. Franklin <irishlep84@gmail.com>
2017-06-13 14:00:40 -0500

    Checkpoint 28 Complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612133833) do
+ActiveRecord::Schema.define(version: 20170613155437) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20170612133833) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
